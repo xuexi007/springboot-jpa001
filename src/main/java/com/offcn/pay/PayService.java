4f6cd34291a8b4802cc3dd3a9d05b1e7e1b6245e
sunny <test@qq.com>
2020-01-08 13:37:55 +0800

    开发支付模块： 新增功能：支付、修改、删除

---
+++ src/main/java/com/offcn/pay/PayService.java
@@ -0,0 +1,10 @@
+package com.offcn.pay;
+
+public interface PayService {
+
+    public void add(Double money);
+
+    public void update(Double money);
+
+    public void delete(Long id);
+}
---
+++ src/main/java/com/offcn/pay/impl/PayServiceImpl.java
@@ -0,0 +1,20 @@
+package com.offcn.pay.impl;
+
+import com.offcn.pay.PayService;
+
+public class PayServiceImpl implements PayService {
+    @Override
+    public void add(Double money) {
+        System.out.println("支付增加");
+    }
+
+    @Override
+    public void update(Double money) {
+        System.out.println("支付更新");
+    }
+
+    @Override
+    public void delete(Long id) {
+        System.out.println("支付删除");
+    }
+}
